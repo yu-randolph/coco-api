89e956afce8766d677bfe36b95939d90c6834f73
Harry Choi <harry124578@naver.com>
2018-03-08 11:34:38 +0800

    FIXING

--- src/main/java/dlsu/coco/coco_api/CocoApiApplication.java
+++ src/main/java/dlsu/coco/coco_api/CocoApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 public class CocoApiApplication {
--- src/main/java/dlsu/coco/coco_api/controller/FileController.java
+++ src/main/java/dlsu/coco/coco_api/controller/FileController.java
@@ -1,80 +1,83 @@
 package dlsu.coco.coco_api.controller;
 
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.Iterator;
-
-import javax.servlet.http.HttpServletResponse;
-
 import dlsu.coco.coco_api.model.UploadedFile;
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/cont")
 public class FileController {
 
-    UploadedFile ufile;
+    UploadedFile file;
 
-    public FileController()
-    {
-        System.out.println("init RestController");
-        ufile = new UploadedFile();
+    @GetMapping("/")
+    public String index() {
+        return "WELCOME";
     }
 
-    @RequestMapping(value = "/get/{value}", method = RequestMethod.GET)
+    @PostMapping("/upload")
+    public @ResponseBody
+    void upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+        Iterator<String> itr = request.getFileNames();
+
+        MultipartFile mpf = request.getFile(itr.next());
+        System.out.println(mpf.getOriginalFilename() + " uploaded!");
+
+        try {
+            //just temporary save file info into ufile
+            file.length = mpf.getBytes().length;
+            file.bytes = mpf.getBytes();
+            file.type = mpf.getContentType();
+            file.name = mpf.getOriginalFilename();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (mpf.getContentType().equals(".xml")) {
+            System.out.println("XML!");
+        } else if (mpf.getContentType().equals(".txt")) {
+            System.out.println("TXT!");
+        }
+    }
+
+    @GetMapping("/get/{value}")
     public void get(HttpServletResponse response, @PathVariable String value)
     {
         try {
 
-            response.setContentType(ufile.type);
-            response.setContentLength(ufile.length);
-            FileCopyUtils.copy(ufile.bytes, response.getOutputStream());
+            response.setContentType(file.type);
+            response.setContentLength(file.length);
+            FileCopyUtils.copy(file.bytes, response.getOutputStream());
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
-
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public @ResponseBody void upload(MultipartHttpServletRequest request, HttpServletResponse response)
-    {
-        Iterator<String> itr =  request.getFileNames();
-
-        MultipartFile mpf = request.getFile(itr.next());
-        System.out.println(mpf.getOriginalFilename() +" uploaded!");
-
-        try {
-            //just temporary save file info into ufile
-            ufile.length = mpf.getBytes().length;
-            ufile.bytes= mpf.getBytes();
-            ufile.type = mpf.getContentType();
-            ufile.name = mpf.getOriginalFilename();
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        if(mpf.getContentType().equals(".xml"))
-        {
-            System.out.println("XML!");
-        }
-        else if(mpf.getContentType().equals(".txt"))
-        {
-            System.out.println("TXT!");
-        }
-
     }
 }
 
