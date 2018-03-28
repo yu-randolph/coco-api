fbe4eeb77f5f6e25e5d98fc87f25ecd92b1871ed
Harry Choi <harry124578@naver.com>
2018-03-29 04:38:58 +0800

    SAMPLE KEYWORDS CONTENT IN JSON TO RUN CODE : 1. INSTALL WORDNET! 2. SET THE DICTLOCTION PATH!

--- src/main/java/dlsu/coco/coco_api/model/Concordancer.java
+++ src/main/java/dlsu/coco/coco_api/model/Concordancer.java
@@ -75,7 +75,7 @@ public class Concordancer {
                     {
                         if(item.equals(word.getWord()))
                         {
-
+                            
                         }
                     }
                 }
